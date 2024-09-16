fce049e2f0958c0fd207d5ad9e6a96f471fd65bc
Jithin Lal K <jithinlalk25@gmail.com>
2024-09-16 16:41:08 +0530

    Update platform fee

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
 export const Constant =
   process.env.ENV == 'prod'
     ? {
-        PLATFORM_FEE: 5,
+        PLATFORM_FEE: 3,
         CASHFREE_API_URL: 'https://api.cashfree.com/pg',
       }
     : {
-        PLATFORM_FEE: 5,
+        PLATFORM_FEE: 3,
         CASHFREE_API_URL: 'https://sandbox.cashfree.com/pg',
       };
